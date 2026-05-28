fa500e19c19e1409048b1079ffa87bad1d6809b1
Mrudul <mrudulchaudhari0608@gmail.com>
2026-05-28 11:58:43 +0530

    tested auth api

--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
 # Create your views here.
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def profile_view(request):
+    return Response({
+        "message": "Authenticated successfully",
+        "email": request.user.email
+    })
